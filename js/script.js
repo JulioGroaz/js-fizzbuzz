7613d97cf03b15e766b05355d6b383b560adc064
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-10 15:10:02 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@ for (let n = 1; n <= 100; n ++){
         console.log("FizzBuzz")
     } else if (n %3 === 0) {
         console.log("Fizz")
-    } 
+    } else if (n %5 === 0){
+        console.log("Buzz")
+    } else{
+        console.log (n)
+    }
 }
